13013cf5be7a0287dfc1c1e4721252a9841434b4
oracle <oracle@victoria24.danielcastelao.org>
2017-11-17 09:48:43 +0100

    B.

--- src/excod1av/ExCOD1Av.java
+++ src/excod1av/ExCOD1Av.java
@@ -16,7 +16,7 @@ public class ExCOD1Av {
      */
     public static void main(String[] args) {
         // TODO code application logic here
-        //A
+        //A VER SI APRUEBO
         
     }
     
